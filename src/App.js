f8c3d4213e1fd02dfde44c407f275a8036c3f107
victorp24 <victor_guapo999@yahoo.com>
2020-09-26 21:01:30 -0700

    added carousel, title, and scroll down components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 // components
 import MyNavbar from './components/my-navbar/mynavbar.component';
+import MyCarousel from './components/my-carousel/my-carousel.component';
+import TitleMessage from './components/title-message/title-message.component';
 
 const App = () => {
   return (
     <div>
       <MyNavbar />
-      My Portfolio
+      <MyCarousel />
+      <TitleMessage />
     </div>
   );
 }
